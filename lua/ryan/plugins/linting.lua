42197dbb58abeae0334a6c3ad66fa932e488e672
Ryan Vu <ryanvu2003@gmail.com>
2024-09-26 10:16:41 -0400

    Update linting and gitignore

--- lua/ryan/plugins/linting.lua
+++ lua/ryan/plugins/linting.lua
@@ -1,28 +1,52 @@
 return {
-	"mfussenegger/nvim-lint",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local lint = require("lint")
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local lint = require("lint")
 
-		lint.linters_by_ft = {
-			javascript = { "eslint", "eslint_d" },
-			typescript = { "eslint", "eslint_d" },
-			javascriptreact = { "eslint", "eslint_d" },
-			typescriptreact = { "eslint", "eslint_d" },
-			svelte = { "eslint_d" },
-		}
+        lint.linters_by_ft = {
+            javascript = { "eslint_d" },
+            typescript = { "eslint_d" },
+            javascriptreact = { "eslint_d" },
+            typescriptreact = { "eslint_d" },
+            svelte = { "eslint_d" },
+            lua = { "luacheck" },
+            python = { "pylint" },
+            -- Add more file types and linters as needed
+        }
 
-		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+        local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-			group = lint_augroup,
-			callback = function()
-				lint.try_lint()
-			end,
-		})
+        vim.api.nvim_create_autocmd({ "BufWritePost", "InsertLeave" }, {
+            group = lint_augroup,
+            callback = function()
+                lint.try_lint()
+            end,
+        })
 
-		vim.keymap.set("n", "<leader>l", function()
-			lint.try_lint()
-		end, { desc = "Trigger linting for current file" })
-	end,
+        vim.keymap.set("n", "<leader>l", function()
+            lint.try_lint()
+        end, { desc = "Trigger linting for current file" })
+
+        vim.keymap.set("n", "<leader>lf", function()
+            lint.try_lint()
+            vim.diagnostic.setloclist({ open = true })
+        end, { desc = "Trigger linting and open diagnostics list" })
+
+        -- Function to get the names of active linters for the current buffer
+        local function get_active_linters()
+            local ft = vim.bo.filetype
+            return lint.linters_by_ft[ft] or {}
+        end
+
+        -- Keymap to show active linters
+        vim.keymap.set("n", "<leader>la", function()
+            local linters = get_active_linters()
+            if #linters > 0 then
+                print("Active linters: " .. table.concat(linters, ", "))
+            else
+                print("No active linters for this file type")
+            end
+        end, { desc = "Show active linters" })
+    end,
 }
